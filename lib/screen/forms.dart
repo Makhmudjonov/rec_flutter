acfc2137d9613978d95e82831a207ac86f4dd5d1
Miraziz <makhmudjonov14@gmail.com>
2025-05-14 17:34:19 +0500

    forms new

--- lib/screen/forms.dart
+++ lib/screen/forms.dart
@@ -13,9 +13,22 @@ class _RectorState extends State<Rector> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        leading: Row(
+          children: [
+            SizedBox(
+              width: 80,
+            ),
+            Image(
+              image: AssetImage("assets/logo.png"),
+              height: 100,
+            ),
+          ],
+        ),
+        leadingWidth: 200,
+        centerTitle: false,
         title: const Text(
           "Rektorga murojaat",
-          style: TextStyle(color: Colors.amber),
+          style: TextStyle(color: Colors.white, fontSize: 50),
         ),
         toolbarHeight: 150,
         backgroundColor: Color(0xFF002D6D),
@@ -75,168 +88,237 @@ class DesktopView extends StatelessWidget {
                 ),
                 color: Colors.white,
                 borderRadius: BorderRadius.circular(8)),
-            child: Column(
-              children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    SizedBox(
-                      width: screenWidth *
-                          0.2, // yoki MediaQuery.of(context).size.width * 0.8
-                      child: TextField(
-                        decoration: InputDecoration(
-                            border: OutlineInputBorder(
-                                borderRadius: BorderRadius.circular(12)),
-                            hintText: 'Ism',
-                            labelText: "Ism"),
-                      ),
-                    ),
-                    SizedBox(
-                      width: 10,
-                    ),
-                    SizedBox(
-                      width: screenWidth *
-                          0.2, // yoki MediaQuery.of(context).size.width * 0.8
-                      child: TextField(
-                        decoration: InputDecoration(
-                            border: OutlineInputBorder(
-                                borderRadius: BorderRadius.circular(12)),
-                            hintText: 'Familiya',
-                            labelText: "Familiya"),
-                      ),
-                    ),
-                    SizedBox(
-                      width: 10,
-                    ),
-                    SizedBox(
-                      width: screenWidth *
-                          0.2, // yoki MediaQuery.of(context).size.width * 0.8
-                      child: TextField(
-                        decoration: InputDecoration(
-                            border: OutlineInputBorder(
-                                borderRadius: BorderRadius.circular(12)),
-                            hintText: 'Sharif',
-                            labelText: "Sharif"),
-                      ),
-                    ),
-                    SizedBox(
-                      width: 10,
-                    ),
-                  ],
-                ),
-                SizedBox(
-                  height: 20,
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    SizedBox(
-                      width: screenWidth *
-                          0.2, // yoki MediaQuery.of(context).size.width * 0.8
-                      child: TextField(
-                        decoration: InputDecoration(
-                            border: OutlineInputBorder(
-                                borderRadius: BorderRadius.circular(12)),
-                            hintText: 'Pochta',
-                            labelText: "Pochta"),
-                      ),
-                    ),
-                    SizedBox(
-                      width: 10,
-                    ),
-                    SizedBox(
-                      width: screenWidth *
-                          0.2, // yoki MediaQuery.of(context).size.width * 0.8
-                      child: IntlPhoneField(
-                        keyboardType: TextInputType.numberWithOptions(),
-                        decoration: InputDecoration(
-                          counterText: '',
-                          labelText: 'Telefon raqam',
-                          border: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(12)),
+            child: SingleChildScrollView(
+              scrollDirection: Axis.vertical,
+              child: Column(
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      SizedBox(
+                        width: screenWidth *
+                            0.2, // yoki MediaQuery.of(context).size.width * 0.8
+                        child: TextField(
+                          decoration: InputDecoration(
+                              border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(12)),
+                              hintText: 'Ism',
+                              labelText: "Ism"),
                         ),
-                        initialCountryCode: 'UZ',
-                        onChanged: (phone) {
-                          print(phone.completeNumber);
-                        },
                       ),
-                    ),
-                    SizedBox(
-                      width: 10,
-                    ),
-                    SizedBox(
-                      width: screenWidth *
-                          0.2, // yoki MediaQuery.of(context).size.width * 0.8
-                      child: DropdownButtonFormField<String>(
-                        decoration: InputDecoration(
-                          labelText: 'Jinsingiz',
-                          border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(12),
+                      SizedBox(
+                        width: 10,
+                      ),
+                      SizedBox(
+                        width: screenWidth *
+                            0.2, // yoki MediaQuery.of(context).size.width * 0.8
+                        child: TextField(
+                          decoration: InputDecoration(
+                              border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(12)),
+                              hintText: 'Familiya',
+                              labelText: "Familiya"),
+                        ),
+                      ),
+                      SizedBox(
+                        width: 10,
+                      ),
+                      SizedBox(
+                        width: screenWidth *
+                            0.2, // yoki MediaQuery.of(context).size.width * 0.8
+                        child: TextField(
+                          decoration: InputDecoration(
+                              border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(12)),
+                              hintText: 'Sharif',
+                              labelText: "Sharif"),
+                        ),
+                      ),
+                      SizedBox(
+                        width: 10,
+                      ),
+                    ],
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      SizedBox(
+                        width: screenWidth *
+                            0.2, // yoki MediaQuery.of(context).size.width * 0.8
+                        child: TextField(
+                          decoration: InputDecoration(
+                              border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(12)),
+                              hintText: 'Pochta',
+                              labelText: "Pochta"),
+                        ),
+                      ),
+                      SizedBox(
+                        width: 10,
+                      ),
+                      SizedBox(
+                        width: screenWidth *
+                            0.2, // yoki MediaQuery.of(context).size.width * 0.8
+                        child: IntlPhoneField(
+                          keyboardType: TextInputType.numberWithOptions(),
+                          decoration: InputDecoration(
+                            counterText: '',
+                            labelText: 'Telefon raqam',
+                            border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(12)),
+                          ),
+                          initialCountryCode: 'UZ',
+                          onChanged: (phone) {
+                            print(phone.completeNumber);
+                          },
+                        ),
+                      ),
+                      SizedBox(
+                        width: 10,
+                      ),
+                      SizedBox(
+                        width: screenWidth *
+                            0.2, // yoki MediaQuery.of(context).size.width * 0.8
+                        child: DropdownButtonFormField<String>(
+                          decoration: InputDecoration(
+                            labelText: 'Jinsingiz',
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(12),
+                            ),
+                          ),
+                          value: "Erkak",
+                          items: ['Erkak', 'Ayol'].map((gender) {
+                            return DropdownMenuItem(
+                              value: gender,
+                              child: Text(gender),
+                            );
+                          }).toList(),
+                          onChanged: (value) {},
+                        ),
+                      ),
+                      SizedBox(
+                        width: 10,
+                      ),
+                    ],
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      SizedBox(
+                        width: screenWidth *
+                            0.62, // yoki MediaQuery.of(context).size.width * 0.8
+                        child: TextField(
+                          decoration: InputDecoration(
+                              border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(12)),
+                              hintText: 'Murojaat mavzusi',
+                              labelText: "Murojaat mavzusi"),
+                        ),
+                      ),
+                    ],
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Row(
+                    children: [
+                      SizedBox(
+                        width: screenWidth *
+                            0.62, // yoki MediaQuery.of(context).size.width * 0.8
+                        child: TextFormField(
+                          maxLines: 10, // yoki null desangiz cheksiz bo'ladi
+                          minLines: 10,
+                          decoration: InputDecoration(
+                            labelText: 'Murojaat kiriting',
+                            hintText: 'Murojaat',
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(12),
+                            ),
+                            contentPadding: EdgeInsets.all(12),
                           ),
                         ),
-                        value: "Erkak",
-                        items: ['Erkak', 'Ayol'].map((gender) {
-                          return DropdownMenuItem(
-                            value: gender,
-                            child: Text(gender),
-                          );
-                        }).toList(),
-                        onChanged: (value) {},
                       ),
-                    ),
-                    SizedBox(
-                      width: 10,
-                    ),
-                  ],
-                ),
-                SizedBox(
-                  height: 20,
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    SizedBox(
-                      width: screenWidth *
-                          0.62, // yoki MediaQuery.of(context).size.width * 0.8
-                      child: TextField(
-                        decoration: InputDecoration(
-                            border: OutlineInputBorder(
-                                borderRadius: BorderRadius.circular(12)),
-                            hintText: 'Murojaat mavzusi',
-                            labelText: "Murojaat mavzusi"),
-                      ),
-                    ),
-                  ],
-                ),
-                SizedBox(
-                  height: 20,
-                ),
-                Row(
-                  children: [
-                    SizedBox(
-                      width: screenWidth *
-                          0.62, // yoki MediaQuery.of(context).size.width * 0.8
-                      child: TextFormField(
-                        maxLines: 10, // yoki null desangiz cheksiz bo'ladi
-                        minLines: 3,
-                        decoration: InputDecoration(
-                          labelText: 'Murojaat kiriting',
-                          hintText: 'Murojaat',
-                          border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(12),
+                    ],
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      InkWell(
+                        onTap: () {},
+                        child: Container(
+                          height: 40,
+                          width: 160,
+                          decoration: BoxDecoration(
+                              color: Color(0xFF002D6D),
+                              borderRadius: BorderRadius.circular(10)),
+                          child: Center(
+                            child: Text(
+                              "Yuborish",
+                              style: TextStyle(color: Colors.white, fontSize: 18),
+                            ),
                           ),
-                          contentPadding: EdgeInsets.all(12),
                         ),
-                      ),
-                    ),
-                  ],
-                )
-              ],
+                      )
+                    ],
+                  )
+                ],
+              ),
             ),
           ),
         ),
         Expanded(
-          child: Text("data"),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Container(
+                padding: EdgeInsets.only(
+                  top: 20,
+                ),
+                margin: EdgeInsets.only(top: 75, right: 50),
+                height: MediaQuery.of(context).size.height * 0.4,
+                // width: MediaQuery.of(context).size.width * 0.8,
+                decoration: BoxDecoration(
+                    border: Border.all(
+                      color: Color(0xFF002D6D),
+                      width: 1.0,
+                    ),
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(8)),
+                child: Column(
+                  children: [
+                    Image(
+                      image: AssetImage("assets/rector.png"),
+                    ),
+                    Text(
+                      "Boymuradov Shuxrat Abdujalilovich",
+                      style: TextStyle(
+                        fontSize: 19,
+                        fontWeight: FontWeight.bold,
+                      ),
+                      textAlign: TextAlign.center,
+                    ),
+                    Text(
+                      "Toshkent davlat tibbiyot Universiteti rektori",
+                      style: TextStyle(
+                        fontSize: 15,
+                        // fontWeight: FontWeight.bold,
+                      ),
+                      textAlign: TextAlign.center,
+                    )
+                  ],
+                ),
+              ),
+            ],
+          ),
           flex: 1,
         )
       ],
